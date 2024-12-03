db0a4c6a1577a60c83df39c7d10963ed1fdb3064
James Iovino <jmsiovino@gmail.com>
2024-12-02 21:22:02 -0500

    added login and profile permissions API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth.models import User
+from rest_framework.authtoken import views
 
 from rest_framework.routers import DefaultRouter
 
@@ -7,6 +10,7 @@ from profiles_api import views
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
 path('hello-view/', views.HelloApiView.as_view()),
